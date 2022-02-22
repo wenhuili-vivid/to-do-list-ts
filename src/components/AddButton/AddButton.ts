5e7180321c446b928a1f70f11eb955ee5c4ade83
wenhuili-vivid <15991890150@163.com>
2022-02-22 13:29:06 +0800

    install gh-pages  && realize a button component

--- src/components/AddButton/AddButton.ts
+++ src/components/AddButton/AddButton.ts
@@ -1,12 +1,20 @@
 import styled from 'styled-components';
 import styles from '../../styles/variables';
 
-const AddButton = styled.button`
-    font-size: 1em;
-    margin: 0.5em;
-    padding: 0.25em 1em;
-    border: 1px solid ${styles.backgroundColor};
-    border-radius: 3px;
+const AddButton = styled.button<{primary: boolean}>`
+  background: ${(props) => (props.primary ? styles.backgroundColor : 'white')};
+  color:  ${(props) => (props.primary ? 'white' : styles.backgroundColor)};
+  font-size: 1em;
+  margin: 0.5em;
+  padding: 0.25em 1em;
+  border: 1px solid ${styles.backgroundColor};
+  border-radius: 3px;
+
+  :disabled {
+    background: ${(props) => (props.primary ? '#e0a3b7' : 'white')};
+    color:  ${(props) => (props.primary ? 'white' : '#e0a3b7')};
+    border: 1px solid #e0a3b7;
+  }
 `;
 
 export default AddButton;
